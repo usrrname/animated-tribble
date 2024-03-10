464d7e694b782ab50fe142d8aa9f36030cd7ec2a
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-03-10 17:36:45 -0400

    chore: updates constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,15 +1,16 @@
 import * as Matter from 'matter-js';
 
 const constants = {
-    Engine: Matter.Engine,
-    World: Matter.World,
     Body: Matter.Body,
     Bodies: Matter.Bodies,
-    Events: Matter.Events,
-    Vector: Matter.Vector,
     Composite: Matter.Composite,
-    Render: Matter.Render,
+    Engine: Matter.Engine,
+    Events: Matter.Events,
     Mouse: Matter.Mouse,
-    MouseConstraint: Matter.MouseConstraint
+    MouseConstraint: Matter.MouseConstraint,
+    Pair: Matter.Pair,
+    Render: Matter.Render,
+    World: Matter.World,
+    Vector: Matter.Vector,
 }
 export default constants;
