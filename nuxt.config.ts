0c83e9250decddc6091cc231bb80fa423c6f1c50
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-03-10 15:38:44 -0400

    ci: fix error:  Exiting due to prerender errors.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    baseURL: process.env.PRODUCTION ? "https://usrrname.github.io/animated-tribble" : "http://localhost:3000",
+    prerender: {
+      crawlLinks: true,
+      failOnError: false, 
+    },
+  },
   devtools: { enabled: true }
 })
